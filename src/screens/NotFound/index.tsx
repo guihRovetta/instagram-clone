002dd899174c830630b7818fa426c3836574e86c
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-09 12:16:25 -0300

    feat: create initial Home and NotFound screens components

---
+++ src/screens/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NotFound = () => {
+  return (
+    <View>
+      <Text>Em desenvolvimento!</Text>
+    </View>
+  );
+};
+
+export default NotFound;
