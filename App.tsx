fb0d12d0bb66cbb27d2faec13919c0d6b5643d27
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-09 11:33:01 -0300

    feat: load custom fonts for the app

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
+import {
+  Arimo_400Regular,
+  Arimo_500Medium,
+  Arimo_600SemiBold,
+  Arimo_700Bold,
+  useFonts,
+} from '@expo-google-fonts/arimo';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Arimo_400Regular,
+    Arimo_500Medium,
+    Arimo_600SemiBold,
+    Arimo_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
