d91d85fd95153e51fec4d4fbb1d7d88d0e62c0d3
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-09 11:42:33 -0300

    feat: create theme file and types for theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    background: '#FAFAFA',
+    text500: '#262626',
+    text400: '#8E8E8E',
+
+    yellow500: '#FEDA77',
+    orange500: '#F58529',
+    pink500: '#DD2A7B',
+    purple500: '#8134AF',
+    blue500: '#515BD4',
+  },
+  fonts: {
+    regular: 'Arimo_400Regular',
+    medium: 'Arimo_500Medium',
+    semibold: 'Arimo_600SemiBold',
+    bold: 'Arimo_700Bold',
+  },
+};
