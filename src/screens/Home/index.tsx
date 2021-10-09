059232b0fabc96b87398f29e090daabf0db103bc
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-09 14:19:13 -0300

    feat: add Header component to Home screen and add initial style

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import Header from '../../components/Header';
+import { Container } from './styles';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <Header />
+    </Container>
   );
 };
 
