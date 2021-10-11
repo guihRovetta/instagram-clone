7b3f54bc8fed4a9b272b2d10e4d7b8256e455e5c
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-10 21:29:41 -0300

    refactor: refactor code to use atomic Post components

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,40 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { PostItem } from '../PostList';
-import UserImage from '../UserImage';
-import {
-  Container,
-  Header,
-  TitleWrapper,
-  NameLocationWrapper,
-  Name,
-  Location,
-  MoreButton,
-  MoreIcon,
-  PostImage,
-  ActionButtonsWrapper,
-  LeftWrapper,
-  ActionLeftButton,
-  RightWrapper,
-  HeartOutlinedIcon,
-  HeartFilledIcon,
-  CommentIcon,
-  SendMessageIcon,
-  ActionRightButton,
-  CollectionIcon,
-  FooterWrapper,
-  LikesAmount,
-  PostDescription,
-  PostUserName,
-  ShowCompleteDescriptionButtonText,
-  ShowAllCommentsButton,
-  ShowAllCommentsText,
-  BottomLineWrapper,
-  ShowTranslationButton,
-  ShowTranslationText,
-  CreatedAt,
-  Separator,
-} from './styles';
+import PostActionButtons from './PostActionButtons';
+import PostFooter from './PostFooter';
+import PostHeader from './PostHeader';
+import PostImage from './PostImage';
+import { Container } from './styles';
 
 export type PostProps = {
   data: PostItem;
@@ -43,97 +14,15 @@ export type PostProps = {
 const Post = ({ data }: PostProps) => {
   const { user, post } = data || {};
 
-  const [isLiked, setIsLiked] = useState(false);
-  const [isAddedToCollection, setIsAddedToCollection] = useState(false);
-
-  const handlePressLikeButton = () => {
-    setIsLiked((previousValue) => !previousValue);
-  };
-
-  const handlePressAddToCollectionButton = () => {
-    setIsAddedToCollection((previousValue) => !previousValue);
-  };
-
   return (
     <Container>
-      <Header>
-        <TitleWrapper>
-          <UserImage
-            hasStory={user?.hasStory}
-            size="small"
-            uri={user?.image?.uri}
-          />
+      <PostHeader user={user} />
 
-          <NameLocationWrapper>
-            <Name>{user?.name}</Name>
-            {user?.location && <Location>{user?.location}</Location>}
-          </NameLocationWrapper>
-        </TitleWrapper>
+      <PostImage image={post?.image} />
 
-        <MoreButton>
-          <MoreIcon name="more-horizontal" />
-        </MoreButton>
-      </Header>
+      <PostActionButtons />
 
-      <PostImage
-        source={{
-          uri: post?.image?.uri,
-        }}
-        resizeMode="cover"
-        aspectRatio={post?.image?.aspectRation || 1}
-      />
-
-      <ActionButtonsWrapper>
-        <LeftWrapper>
-          <ActionLeftButton onPress={handlePressLikeButton}>
-            {!isLiked ? (
-              <HeartOutlinedIcon name="hearto" />
-            ) : (
-              <HeartFilledIcon name="heart" />
-            )}
-          </ActionLeftButton>
-          <ActionLeftButton>
-            <CommentIcon name="chatbubble-outline" />
-          </ActionLeftButton>
-          <ActionLeftButton>
-            <SendMessageIcon name="send" />
-          </ActionLeftButton>
-        </LeftWrapper>
-
-        <RightWrapper>
-          <ActionRightButton onPress={handlePressAddToCollectionButton}>
-            <CollectionIcon
-              name={!isAddedToCollection ? 'bookmark-o' : 'bookmark'}
-            />
-          </ActionRightButton>
-        </RightWrapper>
-      </ActionButtonsWrapper>
-
-      <FooterWrapper>
-        <LikesAmount>{`${post?.likesAmount} curtidas`}</LikesAmount>
-
-        <PostDescription numberOfLines={2}>
-          <PostUserName>{`${user?.name} `}</PostUserName>
-          {`${post?.description} `}
-          <ShowCompleteDescriptionButtonText>
-            mais
-          </ShowCompleteDescriptionButtonText>
-        </PostDescription>
-
-        <ShowAllCommentsButton>
-          <ShowAllCommentsText>
-            {`Ver todos os ${post?.commentsAmount} comentários`}
-          </ShowAllCommentsText>
-        </ShowAllCommentsButton>
-
-        <BottomLineWrapper>
-          <CreatedAt>{post?.createdAt}</CreatedAt>
-          <Separator />
-          <ShowTranslationButton>
-            <ShowTranslationText>Ver tradução</ShowTranslationText>
-          </ShowTranslationButton>
-        </BottomLineWrapper>
-      </FooterWrapper>
+      <PostFooter user={user} post={post} />
     </Container>
   );
 };
