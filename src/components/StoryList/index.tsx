77ee0670ad3b0e3ad45703e7159952f3a76b946e
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-11 16:17:16 -0300

    feat: add useStories custom hook to StoryList component

--- src/components/StoryList/index.tsx
+++ src/components/StoryList/index.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
 
+import { useStories } from '../../services/hooks/stories/useStories';
 import Story from '../Story';
 import { StoryFlatList, StoryFlatListSeparator } from './styles';
 
 export type StoryItemType = {
+  id: string;
   name: string;
   uri: string;
   hasStory?: boolean;
 };
 
-type StoryListProps = {
-  items: StoryItemType[];
-};
+const StoryList = () => {
+  const [page, setPage] = useState(0);
+  const [stories, setStories] = useState<StoryItemType[]>([]);
 
-const StoryList = ({ items }: StoryListProps) => {
+  const { data = [], isLoading, isFetching } = useStories(page);
+
+  useEffect(() => {
+    if (data?.length === 0) return;
+
+    setStories((previousState) => [...previousState, ...data]);
+  }, [data]);
+
+  const handleEndReachedOnStoriesList = () => {
+    if (isLoading || isFetching) return;
+    setPage((previousValue) => previousValue + 1);
+  };
+
+  console.log(stories);
+
   return (
     <StoryFlatList
-      data={items}
+      data={stories}
       keyExtractor={(item) => item?.name}
       renderItem={({ item }) => (
         <Story name={item?.name} uri={item?.uri} hasStory={item?.hasStory} />
       )}
       ItemSeparatorComponent={StoryFlatListSeparator}
+      onEndReached={handleEndReachedOnStoriesList}
+      ListFooterComponent={
+        isLoading || isFetching ? <ActivityIndicator /> : null
+      }
     />
   );
 };
