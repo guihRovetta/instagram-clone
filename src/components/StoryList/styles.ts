029f35b7dc5ad14b79603317ce1395aceb0c5dff
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-12 15:52:41 -0300

    style: adjust style of StoryList component

--- src/components/StoryList/styles.ts
+++ src/components/StoryList/styles.ts
@@ -1,7 +1,8 @@
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
 import { StoryItemType } from '.';
+import { convertHexToRGBA } from '../../utils/converteHexToRGBA';
 
 export const StoryFlatList = styled(
   FlatList as new () => FlatList<StoryItemType>
@@ -14,8 +15,8 @@ export const StoryFlatList = styled(
     paddingBottom: 8,
   },
 })`
-  border-bottom-width: 1px;
-  border-color: ${({ theme }) => theme.colors.gray100};
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-color: ${({ theme }) => convertHexToRGBA(theme.colors.gray200, 0.2)};
 `;
 
 export const StoryFlatListSeparator = styled.View`
