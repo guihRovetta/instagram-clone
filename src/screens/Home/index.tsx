68976f24d198ae3c2192dd48035cdebb6fba149a
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-12 17:03:53 -0300

    feat: use dark mode custom hook to chaneg status bar style based on the theme

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,12 +4,15 @@ import React from 'react';
 import Header from '../../components/Header';
 import PostList from '../../components/PostList';
 import StoryList from '../../components/StoryList';
+import { useDarkMode } from '../../hooks/useDarkMode';
 import { Container } from './styles';
 
 const Home = () => {
+  const { themeMode } = useDarkMode();
+
   return (
     <Container>
-      <StatusBar style="auto" />
+      <StatusBar style={themeMode === 'light' ? 'dark' : 'light'} />
       <Header />
 
       <PostList>
